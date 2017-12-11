db37c0ada811ba057a17aefd22386c0defde9049
JJ D'Aurora <jjdaurora@gmail.com>
2017-12-11 01:03:09 -0500

    interface almost done

--- assets/js/granim.js
+++ assets/js/granim.js
@@ -1,21 +1,32 @@
-// declaring Granim.js instance 
-$('#default-state-cta').on('click', function(event) {
-    event.preventDefault();
-    granimInstance.changeState('default-state');
-    setClass('#default-state-cta')
-});
-$('#violet-state-cta').on('click', function(event) {
-    event.preventDefault();
-    granimInstance.changeState('violet-state');
-    setClass('#violet-state-cta')
-});
-$('#orange-state-cta').on('click', function(event) {
-    event.preventDefault();
-    granimInstance.changeState('orange-state');
-    setClass('#orange-state-cta')
+var granimInstance = new Granim({
+    element: '#canvas-basic',
+    name: 'basic-gradient',
+    direction: 'left-right',
+    opacity: [1, 1],
+    isPausedWhenNotInView: true,
+    states : {
+        "default-state": {
+            gradients: [
+                ['#07090A', '#07090A'],
+                ['#02AAB0', '#00CDAC'],
+                ['#DA22FF', '#9733EE']
+            ]
+        }
+    }
 });
 
-function setClass(element) {
-    $('.canvas-interactive-wrapper a').removeClass('active');
-    $(element).addClass('active');
-};
+var granimInstanceTwo = new Granim({
+    element: '#logo-canvas',
+    direction: 'left-right',
+    opacity: [1, 1],
+    states : {
+        "default-state": {
+            gradients: [
+                ['#000', '#35444A'],
+                ['#35444A', '#000'],
+                ['#000', '#35444A'],
+            ],
+            transitionSpeed: 2000
+        }
+    }
+});
