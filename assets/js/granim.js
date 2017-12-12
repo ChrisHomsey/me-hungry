311f550c9562036f5a504716bdf1ca512120cabc
JJ D'Aurora <jjdaurora@gmail.com>
2017-12-11 21:00:17 -0500

    updates 12/11

--- assets/js/granim.js
+++ assets/js/granim.js
@@ -1,32 +1,19 @@
+
 var granimInstance = new Granim({
     element: '#canvas-basic',
     name: 'basic-gradient',
-    direction: 'left-right',
+    direction: 'diagonal',
     opacity: [1, 1],
     isPausedWhenNotInView: true,
     states : {
         "default-state": {
             gradients: [
-                ['#07090A', '#07090A'],
-                ['#02AAB0', '#00CDAC'],
-                ['#DA22FF', '#9733EE']
+                ['#000', '#566E78'],
+                ['#566E78', '#000'],
+                ['#000', '#566E78']
             ]
+            
         }
     }
 });
 
-var granimInstanceTwo = new Granim({
-    element: '#logo-canvas',
-    direction: 'left-right',
-    opacity: [1, 1],
-    states : {
-        "default-state": {
-            gradients: [
-                ['#000', '#35444A'],
-                ['#35444A', '#000'],
-                ['#000', '#35444A'],
-            ],
-            transitionSpeed: 2000
-        }
-    }
-});
